2a69c00a2b46601dc11ea2607eb65c413b281333
Koppa Péter <koppa96@windowslive.com>
2023-12-02 17:54:37 +0100

    refactor map to array

--- day1/task2/main.go
+++ day1/task2/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/koppa96/adventofcode2023/lib"
-	"github.com/samber/lo"
 	"strings"
 )
 
@@ -17,31 +16,30 @@ func main() {
 	fmt.Println(sum)
 }
 
-var numbers = map[string]int{
-	"zero":  0,
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
+var numbers = [10]string{
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
 }
 
 func firstDigit(str string) (int, error) {
 	runes := []rune(str)
-	patterns := lo.Keys(numbers)
 
 	for i := 0; i < len(runes); i++ {
 		if digit, ok := lib.AsDigit(runes[i]); ok {
 			return digit, nil
 		}
 
-		for _, pattern := range patterns {
+		for value, pattern := range numbers {
 			if strings.HasPrefix(string(runes[i:]), pattern) {
-				return numbers[pattern], nil
+				return value, nil
 			}
 		}
 	}
@@ -51,16 +49,15 @@ func firstDigit(str string) (int, error) {
 
 func lastDigit(str string) (int, error) {
 	runes := []rune(str)
-	patterns := lo.Keys(numbers)
 
 	for i := len(runes) - 1; i >= 0; i-- {
 		if digit, ok := lib.AsDigit(runes[i]); ok {
 			return digit, nil
 		}
 
-		for _, pattern := range patterns {
+		for value, pattern := range numbers {
 			if strings.HasSuffix(string(runes[:i+1]), pattern) {
-				return numbers[pattern], nil
+				return value, nil
 			}
 		}
 	}
